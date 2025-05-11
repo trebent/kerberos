4ec987131e09b3876b0c4402eacf73baf42fc7fb
Måns Thörnvik <thornvik.mans@gmail.com>
2025-05-11 20:27:41 +0200

    add srv

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,82 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/trebent/envparser"
-	_ "github.com/trebent/kerberos/internal/env"
+	"github.com/trebent/kerberos/internal/env"
+	"github.com/trebent/zerologr"
 )
 
+var (
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+)
+
+//nolint:gochecknoinits
 func init() {
-	envparser.Prefix = "KRB"
+	envparser.Prefix = "KRB" //nolint:reassign
 }
 
 func main() {
 	flag.CommandLine.SetOutput(os.Stdout)
-	flag.Usage = func() {
+	flag.Usage = func() { //nolint:reassign
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		flag.CommandLine.PrintDefaults()
-		fmt.Fprintf(flag.CommandLine.Output(), "\n")
-		fmt.Fprintf(flag.CommandLine.Output(), envparser.Help())
+		fmt.Fprint(flag.CommandLine.Output(), "\n")
+		fmt.Fprint(flag.CommandLine.Output(), envparser.Help())
 	}
 
 	flag.Parse()
 	// ExitOnError = true
 	_ = envparser.Parse()
 
+	readTimeout = time.Duration(env.ReadTimeoutSeconds.Value()) * time.Second
+	writeTimeout = time.Duration(env.WriteTimeoutSeconds.Value()) * time.Second
+
 	// Set up monitoring
+	logger := zerologr.New(&zerologr.Opts{
+		Console: env.LogToConsole.Value(),
+		Caller:  true,
+		V:       env.LogVerbosity.Value(),
+	})
+
+	// Start Kerberos API GW server
+	if err := startServer(); !errors.Is(err, http.ErrServerClosed) {
+		logger.Error(err, "Failed to start Kerberos HTTP server")
+		os.Exit(1)
+	}
+}
+
+func startServer() error {
+	handler := http.NewServeMux()
+
+	server := http.Server{
+		Addr:         fmt.Sprintf(":%d", env.Port.Value()),
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		Handler:      handler,
+	}
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- server.ListenAndServe()
+	}()
+
+	signalCtx, signalCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer signalCancel()
+	<-signalCtx.Done()
+
+	timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), readTimeout+writeTimeout)
+	defer timeoutCancel()
+
+	return errors.Join(server.Shutdown(timeoutCtx), <-errChan)
 }
--- internal/env/env.go
+++ internal/env/env.go
@@ -7,13 +7,49 @@ import (
 )
 
 var (
-	port = envparser.Register(&envparser.Opts[int]{
+	Port = envparser.Register(&envparser.Opts[int]{
 		Name:  "PORT",
 		Desc:  "Port for the Kerberos API GW server.",
-		Value: 30000,
+		Value: 30000, //nolint:mnd
 		Validate: func(v int) error {
 			if v < 1000 || v > 65535 {
 				return fmt.Errorf("must be between 1000 and 65535: %d", v)
+			}
+			return nil
+		},
+	})
+	LogToConsole = envparser.Register(&envparser.Opts[bool]{
+		Name: "LOG_TO_CONSOLE",
+		Desc: "Set to log to console.",
+	})
+	LogVerbosity = envparser.Register(&envparser.Opts[int]{
+		Name: "LOG_VERBOSITY",
+		Desc: "Set the log verbosity.",
+		Validate: func(v int) error {
+			if v < 0 {
+				return fmt.Errorf("must be greater than or equal to 0: %d", v)
+			}
+			return nil
+		},
+	})
+	ReadTimeoutSeconds = envparser.Register(&envparser.Opts[int]{
+		Name:  "READ_TIMEOUT_SECONDS",
+		Desc:  "Read timeout in seconds.",
+		Value: 5,
+		Validate: func(v int) error {
+			if v < 1 {
+				return fmt.Errorf("must be greater than 0: %d", v)
+			}
+			return nil
+		},
+	})
+	WriteTimeoutSeconds = envparser.Register(&envparser.Opts[int]{
+		Name:  "WRITE_TIMEOUT_SECONDS",
+		Desc:  "Write timeout in seconds.",
+		Value: 5,
+		Validate: func(v int) error {
+			if v < 1 {
+				return fmt.Errorf("must be greater than 0: %d", v)
 			}
 			return nil
 		},
