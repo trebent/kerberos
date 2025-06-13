c0a6c2f96f6ec8fef49ba6f236c8f67510275b8c
Måns Thörnvik <thornvik.mans@gmail.com>
2025-06-13 22:52:13 +0200

    remove unused test code, add test of span propagation

---
+++ test/tracing/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	krbotel "github.com/trebent/kerberos/internal/otel"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+func main() {
+	shutdown, err := krbotel.Instrument(context.TODO(), "tracing-testing-client", "0.1.0")
+	if err != nil {
+		println("Error initializing OpenTelemetry:", err.Error())
+		return
+	}
+	defer shutdown(context.Background())
+
+	println("Running client")
+
+	ctx, span := otel.Tracer("client").Start(context.Background(), "client-request")
+	defer span.End()
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:15000/echo?message=Hello%20World%21&delay=1000", nil)
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
+
+	c := http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		println("Error on GETing towards test server", err.Error())
+	}
+	resp.Body.Close()
+	println("GET request completed with status code:", resp.StatusCode)
+}
