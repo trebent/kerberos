09cb0db1a2199dab3a89481280356a7c06202489
Måns Thörnvik <thornvik.mans@gmail.com>
2025-11-10 09:02:18 +0100

    expand config register

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,19 +2,31 @@ package config
 
 type (
 	Map interface {
-		Register(name string, cfg any) error
+		Register(name string, cfg any, schemaPath string) error
 		Resolve() error
 		Access(name string) (any, error)
 	}
+
+	configEntry struct {
+		schemaPath string
+		cfg        any
+	}
+
 	impl struct {
+		configEntries map[string]*configEntry
 	}
 )
 
+const NoSchema = "no-schema"
+
 func New() Map {
-	return &impl{}
+	return &impl{
+		configEntries: make(map[string]*configEntry),
+	}
 }
 
-func (c *impl) Register(name string, cfg any) error {
+func (c *impl) Register(name string, cfg any, schemaPath string) error {
+	c.configEntries[name] = &configEntry{schemaPath, cfg}
 	return nil
 }
 
