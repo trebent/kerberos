ed79ac110e9baebab60d2b6fbd1639ea627ae5e8
Måns Thörnvik <thornvik.mans@gmail.com>
2026-02-08 18:23:29 +0100

    switch to the more secure bcrypt password hashing function

--- go.mod
+++ go.mod
@@ -71,6 +71,7 @@ require (
 	go.opentelemetry.io/otel/sdk/log v0.16.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.9.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.3 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/exp v0.0.0-20260112195511-716be5621a96 // indirect
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
--- internal/auth/util/password.go
+++ internal/auth/util/password.go
@@ -2,12 +2,14 @@ package util
 
 import (
 	"crypto/rand"
-	"crypto/sha512"
 	"encoding/hex"
 	"io"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
+
+const saltBytes = 32
 
 // MakePassword creates a random password if the input password is "". It will return the input/generated
 // password, the salt, and the hashed version of the password.
@@ -15,27 +17,27 @@ func MakePassword(password string) (string, string, string) {
 	if password == "" {
 		password = uuid.NewString()
 	}
-	hash := sha512.New()
 
-	const saltBytes = 32
 	salt := make([]byte, saltBytes)
 	_, err := io.ReadFull(rand.Reader, salt)
 	if err != nil {
 		panic(err)
 	}
 
-	_, _ = hash.Write(salt)
-	_, _ = hash.Write([]byte(password))
-	return password, hex.EncodeToString(salt), hex.EncodeToString(hash.Sum(nil))
+	hash, err := bcrypt.GenerateFromPassword(append(salt, []byte(password)...), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	return password, hex.EncodeToString(salt), hex.EncodeToString(hash)
 }
 
 // PasswordMatch verifies if the input cleartext password matches the salt and hashed password.
 func PasswordMatch(salt, hashedPassword, clearTextPassword string) bool {
 	decodedSalt, _ := hex.DecodeString(salt)
-	hash := sha512.New()
-	_, _ = hash.Write(decodedSalt)
-	_, _ = hash.Write([]byte(clearTextPassword))
-	inputHashed := hex.EncodeToString(hash.Sum(nil))
-
-	return inputHashed == hashedPassword
+	hashedPasswordBytes, _ := hex.DecodeString(hashedPassword)
+	return bcrypt.CompareHashAndPassword(
+		hashedPasswordBytes,
+		append(decodedSalt, []byte(clearTextPassword)...),
+	) == nil
 }
