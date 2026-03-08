7349505c14c9364cdb0f1786c1dc43b298fa062d
Måns Thörnvik <thornvik.mans@gmail.com>
2026-03-08 18:02:25 +0100

    fix integration test

--- test/integration/admin_api_test.go
+++ test/integration/admin_api_test.go
@@ -7,7 +7,7 @@ import (
 	adminapi "github.com/trebent/kerberos/ft/client/admin"
 )
 
-func TestAuthAdminSuperuser(t *testing.T) {
+func TestAdminLoginSuperuser(t *testing.T) {
 	t.Log("Logging the superuser in")
 	superLoginResp, err := adminClient.LoginSuperuserWithResponse(
 		t.Context(),
@@ -26,7 +26,7 @@ func TestAuthAdminSuperuser(t *testing.T) {
 	verifyStatusCode(superLogoutResp.StatusCode(), http.StatusNoContent, t)
 }
 
-func TestAuthAdminLoginFailure(t *testing.T) {
+func TestAdminLoginSuperuserFailure(t *testing.T) {
 	superLoginResp, err := adminClient.LoginSuperuserWithResponse(
 		t.Context(),
 		adminapi.LoginSuperuserJSONRequestBody{ClientId: "client-id", ClientSecret: "not-correct"},
@@ -44,9 +44,79 @@ func TestAuthAdminLoginFailure(t *testing.T) {
 	}
 }
 
-func TestAuthAdminOASFailure(t *testing.T) {
+func TestAdminOASFailure(t *testing.T) {
 	badSuperLoginResp, err := adminClient.LoginSuperuserWithResponse(t.Context(), adminapi.LoginSuperuserJSONRequestBody{})
 	checkErr(err, t)
 	verifyStatusCode(badSuperLoginResp.StatusCode(), http.StatusBadRequest, t)
 	verifyAdminAPIErrorResponse(badSuperLoginResp.JSON400, t)
+}
+
+func TestAdminGetFlow(t *testing.T) {
+	superLoginResp, err := adminClient.LoginSuperuserWithResponse(
+		t.Context(),
+		adminapi.LoginSuperuserJSONRequestBody{ClientId: superUserClientID, ClientSecret: superUserClientSecret},
+	)
+	checkErr(err, t)
+	verifyStatusCode(superLoginResp.StatusCode(), http.StatusNoContent, t)
+	superSession := extractSession(superLoginResp.HTTPResponse, t)
+
+	getFlowResp, err := adminClient.GetFlowWithResponse(
+		t.Context(),
+		adminapi.RequestEditorFn(requestEditorSessionID(superSession)),
+	)
+	checkErr(err, t)
+	verifyStatusCode(getFlowResp.StatusCode(), http.StatusOK, t)
+
+	for i, component := range *getFlowResp.JSON200 {
+		t.Logf("Flow component index: %d name: %s", i, component.Name)
+
+		switch component.Name {
+		case "observability":
+			if i != 0 {
+				t.Error("observability component should have index 0")
+			}
+		case "router":
+			if i != 1 {
+				t.Error("router component should have index 1")
+			}
+			v := component.Data["backends"]
+			dv := v.([]any)
+			if len(dv) != 2 {
+				t.Errorf("router component should have 2 backends, but got %v", len(dv))
+			}
+		case "custom":
+			if i != 2 {
+				t.Error("custom component should have index 2")
+			}
+			v := component.Data["component_count"]
+			count := v.(float64)
+			if count != 2 {
+				t.Errorf("custom component should have component_count 2, but got %v", count)
+			}
+		case "authorizer":
+			if i != 3 {
+				t.Error("authorizer component should have index 3")
+			}
+			v := component.Data["order"]
+			order := v.(float64)
+			if order != 1 {
+				t.Errorf("authorizer component should have order 1, but got %v", order)
+			}
+		case "oas-validator":
+			if i != 4 {
+				t.Error("oas-validator component should have index 4")
+			}
+			v := component.Data["order"]
+			order := v.(float64)
+			if order != 100 {
+				t.Errorf("oas-validator component should have order 100, but got %v", order)
+			}
+		case "forwarder":
+			if i != 5 {
+				t.Error("forwarder component should have index 5")
+			}
+		default:
+			t.Errorf("Unexpected flow component name: %s", component.Name)
+		}
+	}
 }
