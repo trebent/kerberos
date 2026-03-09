a3ade5120cd5bbb42d1e900d2b86e43b1794b71c
Måns Thörnvik <thornvik.mans@gmail.com>
2026-03-09 21:32:26 +0100

    fix config schema

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestConfigBad(t *testing.T) {
+	// zerologr.Set(zerologr.New(&zerologr.Opts{V: 100, Console: true}))
+	// defer func() {
+	// 	zerologr.Set(zerologr.New(&zerologr.Opts{V: 0}))
+	// }()
+
+	data, err := os.ReadFile("./testconfig/bad.json")
+	if err != nil {
+		t.Fatalf("failed to read test config: %v", err)
+	}
+
+	cfg := New()
+	cfg.Load(data)
+	if err := cfg.Parse(); err == nil {
+		t.Fatalf("expected error when loading bad config, got nil")
+	}
+}
+
 func TestConfigReferences(t *testing.T) {
 	// zerologr.Set(zerologr.New(&zerologr.Opts{V: 100, Console: true}))
 	// defer func() {
