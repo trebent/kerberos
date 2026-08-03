ed265246becbf2649d558bcff6fb3d8d436137a7
maansaake <mans.thornvik@pm.me>
2026-08-03 21:48:33 +0200

    comment router CORS

--- internal/composer/router/cors.go
+++ internal/composer/router/cors.go
@@ -8,6 +8,7 @@ import (
 	apierror "github.com/trebent/kerberos/internal/oapi/error"
 )
 
+// corsConfigured returns true if the backend has CORS configured, false otherwise.
 func corsConfigured(backend *config.RouterBackend) bool {
 	if backend.Origins == nil {
 		return false
@@ -18,6 +19,9 @@ func corsConfigured(backend *config.RouterBackend) bool {
 		len(backend.Origins.AllowedOrigins) > 0
 }
 
+// ValidateOrigin validates the origin against the configured origins. It returns
+// an error if the origin is not allowed. The error is of type [apierror.APIError]
+// and can be used to return a proper HTTP response.
 func ValidateOrigin(origin string, originsCfg *config.Origins) error {
 	if originsCfg.DenyAll {
 		return apierror.New(http.StatusForbidden, "origin denied")
